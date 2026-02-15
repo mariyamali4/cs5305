4b6354c411fdcb0a2459d847cfece44e610c165f
akhawaja <ali.khawaja@outlook.com>
2026-02-15 22:19:15 +0500

    comit changes

--- module5-structured-streaming/1.submit-job.py
+++
@@ -1,9 +0,0 @@
-
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.remote("sc://localhost:15002").getOrCreate()
-
-data = [(1, "Ali"), (2, "Sara")]
-df = spark.createDataFrame(data, ["id", "name"])
-df.write.mode("overwrite").parquet("file:///tmp/out_parquet")
-df.show()
-spark.stop()
