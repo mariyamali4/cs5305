a712691e3d8c7fd93bed54ca9a2748797ae5d325
akhawaja <ali.khawaja@outlook.com>
2026-02-17 13:22:23 +0500

    updated entrypoint

--- spark_image/entrypoint.sh
+++ spark_image/entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$SPARK_MODE" = "MASTER" ]; then
     # Start Spark History Server
     /opt/spark/sbin/start-history-server.sh --properties-file /opt/spark/conf/spark-history-server.conf &
     # Start Jupyter Server
-    jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
+    #jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
 else
     # Start Spark Worker
     /opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker  --properties-file /opt/spark/conf/spark-worker.conf spark://spark-master:7077
