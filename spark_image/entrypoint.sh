03ba7d66c78b60ee7e4bf0b4023fec2d546843ca
akhawaja <ali.khawaja@outlook.com>
2026-02-17 12:19:06 +0500

    updated docker file and compose

--- spark_image/entrypoint.sh
+++ spark_image/entrypoint.sh
@@ -5,7 +5,9 @@ if [ "$SPARK_MODE" = "MASTER" ]; then
     # Start Spark Connect Server
     /opt/spark/sbin/start-connect-server.sh --properties-file /opt/spark/conf/spark-connect-server.conf &
     # Start Spark History Server
-    /opt/spark/sbin/start-history-server.sh --properties-file /opt/spark/conf/spark-history-server.conf
+    /opt/spark/sbin/start-history-server.sh --properties-file /opt/spark/conf/spark-history-server.conf &
+    # Start Jupyter Server
+    jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
 else
     # Start Spark Worker
     /opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker  --properties-file /opt/spark/conf/spark-worker.conf spark://spark-master:7077
